f15b50b5b848c610d39c6328c781285ba0cadd65
wangjm <wangjm@123>
2022-09-19 10:55:21 +0800

    添加pagehelper的依赖

--- life-start-parent/mybatis-start-life/src/main/java/com/authorization/mybatis/start/MybatisPlusConfig.java
+++ life-start-parent/mybatis-start-life/src/main/java/com/authorization/mybatis/start/MybatisPlusConfig.java
@@ -1,10 +1,19 @@
 package com.authorization.mybatis.start;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusPropertiesCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.github.pagehelper.PageInterceptor;
+import org.apache.ibatis.plugin.Interceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Objects;
 
 /**
  * <p>
@@ -17,16 +26,33 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 public class MybatisPlusConfig {
+
     @Bean
-    public MybatisPlusInterceptor paginationInterceptor() {
-        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor());
-        return mybatisPlusInterceptor;
+    public MybatisPlusPropertiesCustomizer plusPropertiesCustomizer(Environment environment) {
+        return plusProperties -> {
+            plusProperties.getGlobalConfig().setBanner(false);
+            if (Objects.isNull(environment.getProperty("mybatis-plus.global-config.db-config.id-type"))) {
+                plusProperties.getGlobalConfig().getDbConfig().setIdType(IdType.AUTO);
+            }
+        };
     }
 
     @Bean
-    public PaginationInnerInterceptor paginationInnerInterceptor() {
-        return new PaginationInnerInterceptor();
+    @Order(100)
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 防止全表更新
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        // 添加乐观锁控制
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+
+    @Bean
+    @Order(200)
+    public Interceptor pageInterceptor() {
+        // 配置分页插件
+        return new PageInterceptor();
     }
 
 
--- life-system/src/test/java/com/authorization/test/SystemApplicationTest.java
+++ life-system/src/test/java/com/authorization/test/SystemApplicationTest.java
@@ -1,25 +1,23 @@
 package com.authorization.test;
 
-import com.authorization.redis.start.util.ObjRedisHelper;
 import com.authorization.system.SystemLifeApplication;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Slf4j
 @SpringBootTest(classes = SystemLifeApplication.class)
 public class SystemApplicationTest {
 
     @Autowired
-    private ObjRedisHelper objRedisHelper;
+    private RedisTemplate<String, Object> redisTemplate;
     public static final String AUTHORIZATION = "oauth-server:auth:oauth2:authorization";
 
 
     @Test
     public void preRedisTemp() {
-
-        objRedisHelper.hSet(AUTHORIZATION,"98765431321741852369852147","");
 
     }
 

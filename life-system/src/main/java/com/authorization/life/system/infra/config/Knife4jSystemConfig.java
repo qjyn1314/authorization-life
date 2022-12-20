0387487ba51d2f7238369c6e9faee4491307473e
wangjunming <qjyn1314@163.com>
2022-12-20 19:58:57 +0800

    修改swagger的配置

--- life-system/src/main/java/com/authorization/life/system/infra/config/Knife4jSystemConfig.java
+++ life-system/src/main/java/com/authorization/life/system/infra/config/Knife4jSystemConfig.java
@@ -1,18 +1,12 @@
 package com.authorization.life.system.infra.config;
 
-import cn.hutool.core.util.RandomUtil;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.media.StringSchema;
-import io.swagger.v3.oas.models.parameters.HeaderParameter;
 import org.springdoc.core.GroupedOpenApi;
-import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * System服务的文档配置类
@@ -23,49 +17,35 @@ import java.util.Map;
 @Configuration
 public class Knife4jSystemConfig {
 
-    /**
-     * 根据@Tag 上的排序，写入x-order
-     *
-     * @return the global open api customizer
-     */
-    @Bean
-    public GlobalOpenApiCustomizer orderGlobalOpenApiCustomizer() {
-        return openApi -> {
-            if (openApi.getTags() != null) {
-                openApi.getTags().forEach(tag -> {
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("x-order", RandomUtil.randomInt(0, 100));
-                    tag.setExtensions(map);
-                });
-            }
-            if (openApi.getPaths() != null) {
-                openApi.addExtension("x-test123", "333");
-                openApi.getPaths().addExtension("x-abb", RandomUtil.randomInt(1, 100));
-            }
-        };
-    }
+    private static final String PATHS_TO_MATCH = "/**";
+
+    private static final String[] PACKAGED_TO_MATCH = {"com.authorization.life.system.api.controller"};
 
     @Bean
     public GroupedOpenApi userApi() {
-        String[] paths = {"/**"};
-        String[] packagedToMatch = {"com.authorization.life.system.api.controller"};
-        return GroupedOpenApi.builder().group("用户模块").pathsToMatch(paths).addOperationCustomizer((operation, handlerMethod) ->
-                operation.addParametersItem(new HeaderParameter()
-                .name("groupCode").example("测试")
-                .description("集团code").schema(new StringSchema()
-                        ._default("BR").name("groupCode").description("集团code")))).packagesToScan(packagedToMatch).build();
+        return GroupedOpenApi.builder()
+                .group("系统配置模块")
+                .pathsToMatch(PATHS_TO_MATCH)
+                .packagesToScan(PACKAGED_TO_MATCH).build();
     }
 
+    /**
+     * 本工程 访问 /doc.html, 主页显示的内容说明
+     *
+     * @return OpenAPI
+     */
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI systemOpenAPI() {
         return new OpenAPI().info(new Info()
-                .title("XXX用户系统API")
+                .title("系统配置API")
+                .description("关于系统的字典数据,全局样式的配置.")
+                .contact(new Contact()
+                        .name("wangjunming"))
                 .version("1.0")
-                .description("Knife4j集成springdoc-openapi示例")
-                .termsOfService("http://doc.xiaominfo.com")
+                .termsOfService("https://doc.authorization.life/system-life")
                 .license(new License()
                         .name("Apache 2.0")
-                        .url("http://doc.xiaominfo.com")));
+                        .url("https://doc.authorization.life")));
     }
 
 }

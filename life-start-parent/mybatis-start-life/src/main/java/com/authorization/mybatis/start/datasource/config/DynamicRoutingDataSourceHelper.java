92c8c13e9da25cc6ef2256ca7b0e2d2ed2874b72
wangjunming <qjyn1314@163.com>
2023-02-25 20:22:22 +0800

    修改动态数据源配置

--- life-start-parent/mybatis-start-life/src/main/java/com/authorization/mybatis/start/datasource/config/DynamicRoutingDataSourceHelper.java
+++
@@ -1,9 +0,0 @@
-package com.authorization.mybatis.start.datasource.config;
-
-import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
-
-public class DynamicRoutingDataSourceHelper extends DynamicRoutingDataSource {
-
-
-
-}

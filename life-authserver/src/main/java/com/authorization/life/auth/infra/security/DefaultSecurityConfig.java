a1bc1acfe55b088bd174b80bd8f07087aeed5a30
wangjunming <qjyn1314@163.com>
2023-12-16 18:50:04 +0800

    修改security的配置信息

--- life-authserver/src/main/java/com/authorization/life/auth/infra/security/DefaultSecurityConfig.java
+++ life-authserver/src/main/java/com/authorization/life/auth/infra/security/DefaultSecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
@@ -70,53 +71,57 @@ public class DefaultSecurityConfig {
                                                           JwtAuthenticationFilter jwtAuthenticationFilter,
                                                           AuthenticationProvider usernamePasswordProvider)
             throws Exception {
-        http
-                // 前后端分离工程需要 禁用csrf-取消csrf防护-参考：https://blog.csdn.net/yjclsx/article/details/80349906
-                .csrf().disable()
-                .sessionManagement()
-                //限制同一账号只能一个用户使用
+
+        // 前后端分离工程需要 禁用csrf-取消csrf防护-参考：https://blog.csdn.net/yjclsx/article/details/80349906
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // 配置session会话管理器
+        http.sessionManagement(sessionMan -> sessionMan
+                // 此处必须配置为  security 在需要的时候创建session, 因为 OAuth2AuthorizationServer 时通过当前session会话中获取登录用户信息的.
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                // 仅允许一个用户登录
                 .maximumSessions(1)
-                //会话过期后的配置
-                .expiredSessionStrategy(new TokenInformationExpiredStrategy());
-                /*
-                Spring Security下的枚举SessionCreationPolicy,管理session的创建策略
-                ALWAYS
-                    总是创建HttpSession
-                IF_REQUIRED
-                    Spring Security只会在需要时创建一个HttpSession
-                NEVER
-                    Spring Security不会创建HttpSession，但如果它已经存在，将可以使用HttpSession
-                STATELESS
-                    Spring Security永远不会创建HttpSession，它不会使用HttpSession来获取SecurityContext
-                 */
-        // 使用session
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
-        // 设置认证的路径
-        http.authorizeHttpRequests()
+                // session过期后的处理策略
+                .expiredSessionStrategy(new TokenInformationExpiredStrategy()));
+
+        http.authorizeHttpRequests(authHttpReq -> authHttpReq
                 // 无需认证即可访问
                 .requestMatchers(SecurityConstant.IGNORE_PERM_URLS).permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated()
-                .and()
-                .exceptionHandling()
-                //未登录时请求访问接口所需要跳转的自定义路径，即没有登录时将直接跳转到此 url中
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint());
+        );
+
+        // 配置异常情况的处理器
+        http.exceptionHandling(exceHandle ->
+                exceHandle
+                        //未登录时请求访问接口所需要跳转的自定义路径，即没有登录时将直接跳转到此 url中
+                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint()));
+
         // 配置退出登录配置
-        http.logout()
+        http.logout(logout -> logout
+                // 指定退出登录url
                 .logoutUrl(SecurityConstant.SSO_LOGOUT)
+                // 自定义退出登录处理器,根据当前登录用户的信息删除缓存中的数据
                 .addLogoutHandler(new SsoLogoutHandle(authorizationService, stringRedisService, ssoSecurityProperties))
                 //在此处可以删除相应的cookie
                 .deleteCookies(SecurityConstant.JSESSIONID)
+                //需要验证session
                 .invalidateHttpSession(true)
-                .clearAuthentication(true);
+                //清楚认证权限
+                .clearAuthentication(true));
+
         // 配置表单登录配置
-        http.formLogin()
+        http.formLogin(form -> form
+                // 指定form表单登录的路径
                 .loginProcessingUrl(SecurityConstant.SSO_LOGIN)
+                // 配置 达到登录失败次数时, 需要填写的验证码等信息
                 .authenticationDetailsSource(authenticationDetailsSource)
+                // 表单登录成功处理器
                 .successHandler(new SsoSuccessHandler())
-                .failureHandler(new SsoFailureHandler());
-        // 添加jwtfilter  过滤器顺序为 jwtAuthenticationFilter -> UsernamePasswordFilter
+                // 表单登录失败处理器, 用于返回
+                .failureHandler(new SsoFailureHandler())
+        );
+        // 添加jwtfilter  过滤器顺序为 jwtAuthenticationFilter -> UsernamePasswordFilter , 其目的主要是为了每次验证解析jwtToken为当前登录用户并设置到当前sesurity会话中
         http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         // 添加自定义的用户名密码认证类
         http.authenticationProvider(usernamePasswordProvider);

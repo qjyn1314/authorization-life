bb36549e5ffcdc61baeaba45cffe1c23bf752152
wangjunming <qjyn1314@163.com>
2024-08-18 18:46:23 +0800

    添加断路器和负载均衡的参考说明

--- life-gateway/src/main/java/com/authorization/gateway/listener/InstanceChangeSubscriber.java
+++ life-gateway/src/main/java/com/authorization/gateway/listener/InstanceChangeSubscriber.java
@@ -6,16 +6,9 @@ import com.authorization.redis.start.listener.RedisSubscription;
 import com.authorization.utils.contsant.ServerUpDown;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cloud.context.named.NamedContextFactory;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientSpecification;
-import org.springframework.cloud.loadbalancer.cache.LoadBalancerCacheManager;
-import org.springframework.cloud.loadbalancer.core.CachingServiceInstanceListSupplier;
 import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.Topic;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 /**
  * 进行订阅，服务上线频道，
@@ -31,8 +24,6 @@ public class InstanceChangeSubscriber implements RedisSubscription {
     @Autowired
     private RouteService routeService;
     @Autowired
-    private NamedContextFactory<LoadBalancerClientSpecification> contextFactory;
-    @Autowired
     private DynamicRoutingService dynamicRoutingService;
 
     /**
@@ -46,14 +37,6 @@ public class InstanceChangeSubscriber implements RedisSubscription {
         log.debug("[{}] 服务实例变动, 事件为: [{}], 触发路由刷新及实例快照清理", applicationName, instanceChangeHeader);
         dynamicRoutingService.updateRoutesByPublishRefreshRoutesEvent();
         routeService.refreshRoutes(null);
-        // 如果存在服务上下文，则清除实例快照缓存
-        if (contextFactory.getContextNames().contains(applicationName)) {
-            LoadBalancerCacheManager cacheManager = contextFactory.getInstance(applicationName, LoadBalancerCacheManager.class);
-            Cache cache = cacheManager.getCache(CachingServiceInstanceListSupplier.SERVICE_INSTANCE_CACHE_NAME);
-            if (Objects.nonNull(cache)) {
-                cache.evictIfPresent(applicationName);
-            }
-        }
     }
 
     @Override

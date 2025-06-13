2a48aa70b7e1dfba1cc3ba0594a397be0729736c
wangjunming <qjyn1314@163.com>
2025-06-13 23:52:34 +0800

    修改前置路由判断

--- front-auth/src/routers/index.js
+++ front-auth/src/routers/index.js
@@ -1,5 +1,7 @@
-import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import routes from './routes'
+// 用于获取当前登录用户的信息
+import {getToken} from "@/utils/storage-util"
 
 const router = createRouter({
     // 具有井号的路径模式
@@ -17,13 +19,35 @@ const router = createRouter({
     routes
 })
 
+const noRequiredPath = ['/clients','/login','/register','/reset-pwd','/auth-redirect'];
+
+
 router.beforeEach((to, from, next) => {
     if (to.matched.length === 0) {
         // 不存在的路由地址
         next('/404')
     } else {
         window.document.title = (to.query.title ? to.query.title : to.meta.title);
-        next()
+        let accessToken = getToken();
+        let noAuthFlag  = noRequiredPath.includes(to.path);
+        // 没有token, 需要认证的则跳转到client管理
+        if(!accessToken && !noAuthFlag){
+         return next("/clients");
+        }
+        // 不需要登录的路径直接放行
+        if(noAuthFlag) {
+            return next()
+        }
+        // 有登录用户信息直接放行
+        if (accessToken && to.path === '/login') {
+            console.log('getToken()', getToken());
+           return next("/index")
+        }
+        // 有登录用户信息直接放行
+        if (accessToken) {
+            console.log('getToken()', getToken());
+           return next()
+        }
     }
 
 })
